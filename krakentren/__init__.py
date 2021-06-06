6bdff014e644a3f96ad8334642cd895c5a71f116
Ionsper <ionsper@outlook.com>
2021-06-06 22:32:53 +0300

    initial commit

---
+++ krakentren/__init__.py
@@ -0,0 +1,19 @@
+""" Kraken_trade_enhancer
+
+Placeholder
+
+"""
+from .krakentren import (round_down_decimals,
+                         get_server_time,
+                         get_tradable_assets_pairs,
+                         get_asset_pair_info,
+                         get_account_balance,
+                         get_order_info,
+                         order_status,
+                         trade_fee,
+                         trade_cost,
+                         order_price,
+                         order_volume,
+                         cancel_order,
+                         Coin
+                         )
