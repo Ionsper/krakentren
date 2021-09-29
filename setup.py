eb64f2bf5f03d899ce1d8c58cb4eac968cdda05a
Ionsper <Ionsper@outlook.com>
2021-09-29 20:38:59 +0300

    Version 0.11.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="krakentren",
-      version="0.1.0",
+      version="0.11.0",
       description="A python package to interact with Kraken.com REST API",
       url="https://github.com/Ionsper/krakentren",
       author="Ionsper",
